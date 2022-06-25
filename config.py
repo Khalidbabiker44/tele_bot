251fb316696e7fdd0f689ae9c79ffbcd97474af3
Khalidbabiker44 <97848770+Khalidbabiker44@users.noreply.github.com>
2022-06-25 22:15:05 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+
+TOKEN = os.environ["TOKEN"]
+PORT = int(os.environ.get("PORT",5000))
---
+++ main.py
@@ -0,0 +1,22 @@
+from telegram import *
+from telegram.ext import *
+
+from config import PORT,TOKEN
+
+def start(update:Update,context:CallbackContext):
+	update.message.reply_text("hello")
+	
+	
+	
+	
+updater = Updater(TOKEN)
+
+ds = updater.dispatcher
+
+ds.add_handler(CommandHandler("start",start))
+
+
+updater.start_webhook("0,0,0,0",PORT,TOKEN,webhook_url=""+TOKEN)
+updater.idle()
+
+print("running .....")
